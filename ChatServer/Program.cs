1e536e110272caa2c035e6028e84425278fb3f1b
Yeh TzuChen <67541201+uniyeh@users.noreply.github.com>
2021-12-03 19:31:26 +0800

    hand in assignment

---
+++ ChatServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChatServer
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("====================================");
+      var server = new ChatCore.ChatServer();
+      server.Bind(4099);
+      server.Start();
+    }
+  }
+}
---
+++ ReadPackage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class ReadPacket
+{
+	public ReadPacket()
+	{
+	}
+
+
+}
